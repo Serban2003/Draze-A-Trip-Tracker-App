30fe86c453a09dd66dcd8ec95f75a7de60a5d2ae
Iustinian Serban <iustinianserban@gmail.com>
2022-07-30 14:54:04 +0300

    Update Profile Tab based on logged user

--- app/src/main/java/com/example/triptracker/ProfileFragment.java
+++ app/src/main/java/com/example/triptracker/ProfileFragment.java
@@ -1,14 +1,43 @@
 package com.example.triptracker;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 public class ProfileFragment extends Fragment {
+
+    private TextView usernameTextView, emailTextView;
+    private TextView fullNameTextView, genderTextView, phoneTextView, locationTextView;
+    private ImageView avatar;
+    private FirebaseUser user;
+    private FirebaseDatabase database;
+    private DatabaseReference userRef;
+    private String email;
+
+    private final String TAG = this.getClass().getName();
+    private static final String USER = "users";
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,7 +47,66 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+
+        DatabaseReference rootRef = FirebaseDatabase.getInstance("https://trip-tracker-2844c-default-rtdb.europe-west1.firebasedatabase.app/").getReference();
+        DatabaseReference userRef = rootRef.child(USER);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        assert user != null;
+        email = user.getEmail();
+
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        usernameTextView = view.findViewById(R.id.usernameTextView);
+        emailTextView = view.findViewById(R.id.emailTextView);
+        fullNameTextView = view.findViewById(R.id.fullNameTextView);
+        genderTextView = view.findViewById(R.id.genderTextView);
+        phoneTextView = view.findViewById(R.id.phoneTextView);
+        locationTextView = view.findViewById(R.id.locationTextView);
+        avatar = view.findViewById(R.id.avatarIcon);
+
+        userRef.addValueEventListener(new ValueEventListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    if (Objects.equals(ds.child("email").getValue(), email)) {
+                        usernameTextView.setText(Objects.requireNonNull(ds.child("username").getValue()).toString());
+                        emailTextView.setText(Objects.requireNonNull(ds.child("email").getValue()).toString());
+
+                        if (ds.child("fullName").getValue() != null)
+                            fullNameTextView.setText(Objects.requireNonNull(ds.child("fullName").getValue()).toString());
+                        else fullNameTextView.setText("Not provided");
+
+                        if (ds.child("gender").getValue() != null)
+                            genderTextView.setText(Objects.requireNonNull(ds.child("gender").getValue()).toString());
+                        else genderTextView.setText("Not provided");
+
+                        if (ds.child("phoneNumber").getValue() != null)
+                            phoneTextView.setText(Objects.requireNonNull(ds.child("phoneNumber").getValue()).toString());
+                        else phoneTextView.setText("Not provided");
+
+                        if (ds.child("location").getValue() != null)
+                            locationTextView.setText(Objects.requireNonNull(ds.child("location").getValue()).toString());
+                        else locationTextView.setText("Not provided");
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        Activity activity = getActivity();
+        if (activity != null) {
+            activity.setTitle(getString(R.string.profileFragmentTitle));
+        }
     }
 }
